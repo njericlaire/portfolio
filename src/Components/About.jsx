6f528c68525690e556439f1269056b760e741249
njericlaire <132552579+njericlaire@users.noreply.github.com>
2024-05-31 13:25:18 +0000

    changes aim to provide a more engaging and informative user experience.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,26 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/space.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "image of space";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Passionate Third-Year Software Engineering Student in Multimedia University of Kenya| Eager for Opportunities in Tech | Aspiring Developer with a Love for Innovation";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Programming Languages: Python, Java, C++",
+  "Web Technologies: HTML, CSS, JavaScript",
+  "Database Management: MySQL",
+  "Development Tools: Visual Studio Code",
+  "Operating Systems: Windows",
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Motivated and detail-oriented Bachelor of Software Engineering student seeking a challenging attachment opportunity to apply and enhance my programming skills. I have strong problem-solving and analytical skills, excellent communication and teamwork abilities and a quick learner with a passion of staying up-to-date with the industry trends. Eager to contribute to a dynamic team and gain practical experience in software development.";
 
 const About = () => {
   return (
